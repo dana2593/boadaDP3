47059f794f38aa76005c64a7c161b0b6a038c75a
dana259 <dana.boada@udla.edu.ec>
2025-07-07 08:15:43 -0500

    Implementar modelo Equipo y servicio de base de datos SQLite con CRUD operations

---
+++ boadaDP3/Models/Equipo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace boadaDP3.Models
+{
+    public class Equipo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Dispositivo { get; set; } = string.Empty;
+
+        [NotNull]
+        public string Marca { get; set; } = string.Empty;
+
+        public bool GarantiaActiva { get; set; }
+
+        public int VidaUtilMeses { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
