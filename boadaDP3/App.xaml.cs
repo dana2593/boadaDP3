38a746561a650e07f8416005d4d561f6f4c50477
dana259 <dana.boada@udla.edu.ec>
2025-07-07 07:29:49 -0500

    Agregar archivos de proyecto.

---
+++ boadaDP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace boadaDP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
